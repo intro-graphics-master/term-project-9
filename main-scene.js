f8d623b76704d42f63e0f481b33d064ebe3436b8
Ran Gong <gongran911@163.com>
2019-06-02 04:22:51 -0700

    finish weapon

--- main-scene.js
+++ main-scene.js
@@ -1,5 +1,7 @@
 import {tiny, defs} from './assignment-4-resources.js';
 import { Shape_From_File } from './obj-file-demo.js';
+
+
 // import { Obj_File_Demo } from './obj-file-demo.js';
                                                                 // Pull these names into this module's scope for convenience:
 const { Vec, Mat, Mat4, Color, Light, Shape, Shader, Material, Texture,
@@ -18,6 +20,7 @@ var boxes = [];
 var ground_boxes = [];
 var sky_boxes = [];
 var frame = 0;
+var bullet_material;
 //TODO: implement score calculation
  var coinScore = 0;
 //reviving structure
@@ -438,6 +441,47 @@ class mario extends physics_component
       	this.gravity_enabled = true;
 
       	this.jump_count = 0;
+
+      	this.bullets =[];
+      	this.bullets_transform= [];
+      	this.bullet_speed = [];
+      	//new Subdivision_Sphere( 6 );
+
+      	this.bullet_material = bullet_material;
+      	 
+	}
+
+	shoot()
+	{
+		var bullet_transforms;
+		console.log(this.bullets_transform.length);
+		while(this.bullets_transform.length > 5)
+		{
+			this.bullets_transform.shift();
+			this.bullets.shift();
+			this.bullet_speed.shift();	
+		}
+
+		this.update_transform();
+
+		if( Math.abs(this.rotation[1] - Math.PI/2) <= 0.001 )
+		{
+			bullet_transforms = this.transform_position.times(Mat4.translation([1,0,0]));
+			this.bullet_speed.push(1);
+		}
+		else
+		{
+			bullet_transforms = this.transform_position.times(Mat4.translation([-1,0,0]));
+			this.bullet_speed.push(-1);
+		}
+
+		this.bullets_transform.push(bullet_transforms);
+		
+		this.bullets.push(new Subdivision_Sphere( 6 ));
+		
+
+		
+
 	}
 
 
@@ -585,6 +629,8 @@ class mario extends physics_component
 			}
 		}    	
     }
+	
+
 
 
     compute_next()
@@ -697,8 +743,37 @@ class mario extends physics_component
        	 }
        }
 
-       console.log();
     }
+
+    draw(context, program_state, material)
+    {
+	
+			  var i = 0;
+		      this.update_transform();
+		      this.object_type.draw(context, program_state, this.transforms, material);
+			  
+		      for (i = 0 ; i < this.bullets.length; i++)
+		      {
+		      	
+		      	this.bullets[i].draw(context, program_state, this.bullets_transform[i].times(Mat4.translation([2,0,0])), bullet_material);
+		      	this.bullets_transform[i] = this.bullets_transform[i].times(Mat4.translation([0.2*this.bullet_speed[i],0,0]));
+
+		      }
+		      this.compute_next();
+
+	
+
+    }
+}
+
+
+class AI extends mario
+{
+	constructor()
+	{
+		super();
+	}
+
 }
 
 
@@ -709,6 +784,9 @@ class Movement_Controls extends Scene
   constructor()
     {
       super();
+
+
+
       this.shapes = { 'box' : new Cube(),
 	  				'scene_box': new physics_component(1000 ,"cube"),
 					'scene_box_45': new physics_component(1000, "cube",Vec.of(0,0,0), Vec.of(0,0,Math.PI/4)),
@@ -729,16 +807,10 @@ class Movement_Controls extends Scene
 					"interactive_box3":new pushable_box(10, "cube"),
 					"interactive_box4":new pushable_box(10, "cube"),
 					"interactive_box5":new pushable_box(10, "cube"),
-					"plank":new physics_component(100, "cube")
-                      };
-      // *** Shaders ***
-		sky_boxes.push( this.shapes.fixedBox1);
-		sky_boxes.push( this.shapes.fixedBox2);
-		sky_boxes.push( this.shapes.fixedBox3);
-      // NOTE: The 2 in each shader argument refers to the max
-      // number of lights, which must be known at compile time.
+					"plank":new physics_component(100, "cube"),
 
-      // A simple Phong_Blinn shader without textures:
+                      };
+                      
       const phong_shader      = new defs.Phong_Shader  (2);
                                                               // Adding textures to the previous shader:
       const texture_shader    = new defs.Textured_Phong(2);
@@ -771,8 +843,20 @@ class Movement_Controls extends Scene
 			           ambient: 1, diffusivity: 1, specularity: 1, texture: new Texture( "assets/goldcoin.png" ) }),
 
                       black_hole: new Material( black_hole_shader ),
-                             sun: new Material( sun_shader, { ambient: 1, color: Color.of( 0,0,0,1 ) } )
+                             sun: new Material( sun_shader, { ambient: 1, color: Color.of( 1,0.5,0,1 ) } ),
+
                        };
+       bullet_material = this.materials.sun;
+      
+      // *** Shaders ***
+		sky_boxes.push( this.shapes.fixedBox1);
+		sky_boxes.push( this.shapes.fixedBox2);
+		sky_boxes.push( this.shapes.fixedBox3);
+      // NOTE: The 2 in each shader argument refers to the max
+      // number of lights, which must be known at compile time.
+
+      // A simple Phong_Blinn shader without textures:
+
 
                                   // Some setup code that tracks whether the "lights are on" (the stars), and also
                                   // stores 30 random location matrices for drawing stars behind the solar system:
@@ -780,6 +864,8 @@ class Movement_Controls extends Scene
 
       this.move_left = false;
       this.move_right = false;
+      this.shoot = false;
+      this.shot_count = 0;
       this.jump = 0;
 
       this.apply_impulse = 0;
@@ -795,7 +881,7 @@ class Movement_Controls extends Scene
 		// make_control_panel(): Sets up a panel of interactive HTML elements, including
 		// buttons with key bindings for affecting this scene, and live info readouts.
 
-      this.key_triggered_button("apply_impulse", ["1"],  () => {this.apply_impulse += 1;}   );
+      this.key_triggered_button("shoot fireball", ["q"],  () => {this.shoot = true;},'#'+Math.random().toString(9).slice(-6) , () => {this.shoot = false; this.shot_count = 0;}  );
       this.new_line();
       this.key_triggered_button("move_left", ["a"],  () => {this.move_left = true; } , '#'+Math.random().toString(9).slice(-6) , () => {this.move_left = false;}  );
       this.key_triggered_button("move_right", ["d"],  () => {this.move_right = true; } , '#'+Math.random().toString(9).slice(-6) , () => {this.move_right = false;}  );
@@ -803,6 +889,7 @@ class Movement_Controls extends Scene
       this.key_triggered_button("jump", [" "],  () => {this.jump += 1; } , '#'+Math.random().toString(9).slice(-6) );
       this.new_line();
 	  this.key_triggered_button("push", ["f"],  () => {this.push = true; } , '#'+Math.random().toString(9).slice(-6),  );
+
     }
 
 
@@ -868,12 +955,7 @@ class Movement_Controls extends Scene
                                     // Variable model_transform will be a local matrix value that helps us position shapes.
                                     // It starts over as the identity every single frame - coordinate axes at the origin.
       let model_transform = Mat4.identity();
-      const smoothly_varying_ratio = .5 + .5 * Math.sin( 2 * Math.PI * t/10 ),
-            sun_size = 1 + 2 * smoothly_varying_ratio,
-                 sun = undefined,
-           sun_color = undefined;
 
-      this.materials.sun.color = sun_color;     // Assign our current sun color to the existing sun material.
 
                                                 // *** Lights: *** Values of vector or point lights.  They'll be consulted by
                                                 // the shader when coloring shapes.  See Light's class definition for inputs.
@@ -1209,6 +1291,14 @@ function check_for_coin_collection(shapes)
       	this.shapes.mario.push();
       }
 
+      if(this.shoot)
+      {
+      	if( this.shot_count == 0)
+      	this.shapes.mario.shoot(context, program_state, this.materials.sun);
+
+      	this.shot_count = 1;
+      }
+
       var pos = this.shapes.mario.position;
       var factor = 1;
 	  check_for_coin_collection(this.shapes);
@@ -1401,10 +1491,16 @@ class Black_Hole_Shader extends Shader         // Simple "procedural" texture sh
 
 const Sun_Shader = defs.Sun_Shader =
 class Sun_Shader extends Shader
-{ update_GPU( context, gpu_addresses, graphics_state, model_transform, material )
+{ update_GPU( context, gpu_addresses, program_state, model_transform, material )
     {
                       // TODO (#EC 2): Pass the same information to the shader as for EC part 1.  Additionally
                       // pass material.color to the shader.
+                   
+        const [ P, C, M ] = [ program_state.projection_transform, program_state.camera_inverse, model_transform ],
+                      PCM = P.times( C ).times( M );
+        context.uniformMatrix4fv( gpu_addresses.projection_camera_model_transform, false, Mat.flatten_2D_to_1D( PCM.transposed() ) );
+        context.uniform1f ( gpu_addresses.animation_time, program_state.animation_time / 1000 );
+        context.uniform4fv ( gpu_addresses.sun_color,  material.color);
 
 
     }
@@ -1413,22 +1509,138 @@ class Sun_Shader extends Shader
 
   shared_glsl_code()            // ********* SHARED CODE, INCLUDED IN BOTH SHADERS *********
     { return `precision mediump float;
-
+        uniform float animation_time;
+        uniform vec4 sun_color;
+        varying float disp;
+        varying float noise;
+      
+                            
       `;
     }
   vertex_glsl_code()           // ********* VERTEX SHADER *********
     { return this.shared_glsl_code() + `
-
-        void main()
-        {
-
-        }`;
+//         uniform mat4 modelMatrix;
+//         uniform mat4 modelViewMatrix;
+//         uniform mat4 projectionMatrix;
+//         uniform mat4 viewMatrix;
+//         uniform mat3 normalMatrix;
+        uniform mat4 projection_camera_model_transform;
+        attribute vec3 position;
+        attribute vec3 normal;
+        attribute vec2 uv;
+        attribute vec2 uv2;
+        
+   
+        const float fireSpeed = 0.5;
+        const float pulseHeight = 0.5;
+        const float displacementHeight= 0.5;
+        const float turbulenceDetail =0.5;
+        vec3 mod289(vec3 x) {
+          return x - floor(x * (1.0 / 289.0)) * 289.0;
+        }
+        vec4 mod289(vec4 x) {
+          return x - floor(x * (1.0 / 289.0)) * 289.0;
+        }
+        vec4 permute(vec4 x) {
+          return mod289(((x*34.0)+1.0)*x);
+        }
+        vec4 taylorInvSqrt(vec4 r) {
+          return 1.79284291400159 - 0.85373472095314 * r;
+        }
+        vec3 fade(vec3 t) {
+          return t*t*t*(t*(t*6.0-15.0)+10.0);
+        }
+        // Klassisk Perlin noise 
+        float cnoise(vec3 P) {
+          vec3 Pi0 = floor(P); // indexing
+          vec3 Pi1 = Pi0 + vec3(1.0); // Integer part + 1
+          Pi0 = mod289(Pi0);
+          Pi1 = mod289(Pi1);
+          vec3 Pf0 = fract(P); // Fractional part for interpolation
+          vec3 Pf1 = Pf0 - vec3(1.0); // Fractional part - 1.0
+          vec4 ix = vec4(Pi0.x, Pi1.x, Pi0.x, Pi1.x);
+          vec4 iy = vec4(Pi0.yy, Pi1.yy);
+          vec4 iz0 = Pi0.zzzz;
+          vec4 iz1 = Pi1.zzzz;
+          vec4 ixy = permute(permute(ix) + iy);
+          vec4 ixy0 = permute(ixy + iz0);
+          vec4 ixy1 = permute(ixy + iz1);
+          vec4 gx0 = ixy0 * (1.0 / 7.0);
+          vec4 gy0 = fract(floor(gx0) * (1.0 / 7.0)) - 0.5;
+          gx0 = fract(gx0);
+          vec4 gz0 = vec4(0.5) - abs(gx0) - abs(gy0);
+          vec4 sz0 = step(gz0, vec4(0.0));
+          gx0 -= sz0 * (step(0.0, gx0) - 0.5);
+          gy0 -= sz0 * (step(0.0, gy0) - 0.5);
+          vec4 gx1 = ixy1 * (1.0 / 7.0);
+          vec4 gy1 = fract(floor(gx1) * (1.0 / 7.0)) - 0.5;
+          gx1 = fract(gx1);
+          vec4 gz1 = vec4(0.5) - abs(gx1) - abs(gy1);
+          vec4 sz1 = step(gz1, vec4(0.0));
+          gx1 -= sz1 * (step(0.0, gx1) - 0.5);
+          gy1 -= sz1 * (step(0.0, gy1) - 0.5);
+          vec3 g000 = vec3(gx0.x,gy0.x,gz0.x);
+          vec3 g100 = vec3(gx0.y,gy0.y,gz0.y);
+          vec3 g010 = vec3(gx0.z,gy0.z,gz0.z);
+          vec3 g110 = vec3(gx0.w,gy0.w,gz0.w);
+          vec3 g001 = vec3(gx1.x,gy1.x,gz1.x);
+          vec3 g101 = vec3(gx1.y,gy1.y,gz1.y);
+          vec3 g011 = vec3(gx1.z,gy1.z,gz1.z);
+          vec3 g111 = vec3(gx1.w,gy1.w,gz1.w);
+          vec4 norm0 = taylorInvSqrt(vec4(dot(g000, g000), dot(g010, g010), dot(g100, g100), dot(g110, g110)));
+          g000 *= norm0.x;
+          g010 *= norm0.y;
+          g100 *= norm0.z;
+          g110 *= norm0.w;
+          vec4 norm1 = taylorInvSqrt(vec4(dot(g001, g001), dot(g011, g011), dot(g101, g101), dot(g111, g111)));
+          g001 *= norm1.x;
+          g011 *= norm1.y;
+          g101 *= norm1.z;
+          g111 *= norm1.w;
+          float n000 = dot(g000, Pf0);
+          float n100 = dot(g100, vec3(Pf1.x, Pf0.yz));
+          float n010 = dot(g010, vec3(Pf0.x, Pf1.y, Pf0.z));
+          float n110 = dot(g110, vec3(Pf1.xy, Pf0.z));
+          float n001 = dot(g001, vec3(Pf0.xy, Pf1.z));
+          float n101 = dot(g101, vec3(Pf1.x, Pf0.y, Pf1.z));
+          float n011 = dot(g011, vec3(Pf0.x, Pf1.yz));
+          float n111 = dot(g111, Pf1);
+          vec3 fade_xyz = fade(Pf0);
+          vec4 n_z = mix(vec4(n000, n100, n010, n110), vec4(n001, n101, n011, n111), fade_xyz.z);
+          vec2 n_yz = mix(n_z.xy, n_z.zw, fade_xyz.y);
+          float n_xyz = mix(n_yz.x, n_yz.y, fade_xyz.x);
+          return 2.2 * n_xyz;
+        }
+        // Ashima code 
+        float turbulence( vec3 p ) {
+            float t = -0.5;
+            for (float f = 1.0 ; f <= 10.0 ; f++ ){
+                float power = pow( 2.0, f );
+                t += abs( cnoise( vec3( power * p ) ) / power );
+            }
+            return t;
+        }
+        void main() {
+            float a = animation_time;
+            noise = -0.8 * turbulence( turbulenceDetail * normal + ( a * 1.0 ) );
+             float b = pulseHeight * cnoise(
+                 0.05 * position + vec3( 1.0 * a )
+              );
+             float displacement = ( 0.0 - displacementHeight ) * noise + b;
+             vec3 newPosition = position + normal * displacement;
+             gl_Position = projection_camera_model_transform * vec4( newPosition, 2.0 );
+              disp = displacement * 15.0;
+        }
+     `;
     }
   fragment_glsl_code()           // ********* FRAGMENT SHADER *********
     { return this.shared_glsl_code() + `
-        void main()
+        void main() 
         {
-
+            vec3 color = vec3((1.-disp), (0.1-disp*0.2)+0.1, (0.1-disp*0.1)+0.1*abs(sin(disp)));
+            gl_FragColor = vec4( color.rgb, 1.0 );
+            gl_FragColor *= sun_color;
+            //gl_FragColor = vec4(1.0,0.0,0.0,1.0);
         } ` ;
     }
 }
